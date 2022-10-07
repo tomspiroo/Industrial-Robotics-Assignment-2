ad0e0673c07e9e7080c5e2cd7a4f2f101d5d46e1
jeremyjmansfield-UTS <jeremy.j.mansfield@student.uts.edu.au>
2022-10-07 19:15:46 +1100

    Update Test.m

--- Test.m
+++ Test.m
@@ -1,7 +1,17 @@
 %% Test Robot
 robot = Braccio;
 q = [0 0 0 0 0];
-robot.model.plot(q);
-robot.model.teach();
+workspace = [-1 1 0 0.8 -1 1];
+scale = 1;
+robot.model.base = transl(-0.25,0.4,0);
+robot.model.plot(q,'workspace',workspace,'scale',scale);
+%robot.model.teach();
+hold on;
 %% Create Workspace
-
+% Create Tabletop Surface
+ttPoint = [0,0,0];
+ttNormal = [0,-1,0];
+ttvertx = [1,-1;1,-1];
+ttverty = [0,0;0.8,0.8];
+ttvertz = [0,0;0,0];
+tabletop_h = surf(ttvertx,ttverty,ttvertz);
