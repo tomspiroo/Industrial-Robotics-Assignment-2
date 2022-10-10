53bc9f46d7ec200ad46b71266a392cc1b195425e
jeremyjmansfield-UTS <jeremy.j.mansfield@student.uts.edu.au>
2022-10-10 19:05:32 +1100

    Correct minor error in code, add .ply models.

--- Test.m
+++ Test.m
@@ -6,7 +6,7 @@ qur3 = [0 0 0 0 0 0];
 workspace = [-1 1 0 0.8 -1 1];
 scale = 1;
 robot.model.base = transl(-0.25,0.4,0);
-robot.model.plot(qrobot,'workspace',workspace,'scale',scale);
+robot.model.plot(robot,'workspace',workspace,'scale',scale);
 %robot.model.teach();
 hold on;
 ur3.model.base = transl(0.25,0.4,0);
