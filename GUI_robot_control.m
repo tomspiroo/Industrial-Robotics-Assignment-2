f44d13482566083ddf4442013a0d5f82bb1be05e
Thomas Spiritosanto <113651093+tomspiroo@users.noreply.github.com>
2022-10-20 16:18:21 +1100

    Updated GUI and robot motion test

--- GUI_robot_control.m
+++ GUI_robot_control.m
@@ -4,7 +4,7 @@ clear all
 set(0,'DefaultFigureWindowStyle','docked')
 robot = Braccio;
 ur3 = UR3;
-gui = GUI_App
+gui = GUI_App;
 
 qrobot = [0 0 0 0 0];
 qur3 = [0 0 0 0 0 0];
@@ -16,30 +16,37 @@ hold on;
 ur3.model.base = transl(0.25,0.4,0);
 ur3.model.plot(qur3,'workspace',workspace,'scale',scale);
 
-switch gui.PopUpMenu.Value
-    case 'UR3'
-        gui.EditField.Value = 0;
-        gui.EditField_2.Value = 0;
-        gui.EditField_3.Value = 0;
-        gui.EditField_4.Value = 0;
-        gui.EditField_5.Value = 0;
-        gui.EditField_6.Value = 0;
-        while gui.PopUpMenu.Value == "UR3"
-            qur3 = deg2rad([gui.EditField.Value, gui.EditField_2.Value, gui.EditField_3.Value, ... 
-                gui.EditField_4.Value, gui.EditField_5.Value, gui.EditField_6.Value]);
-            ur3.model.plot(qur3);
-        end
-    case 'Braccio'
-        gui.EditField.Value = 0;
-        gui.EditField_2.Value = 0;
-        gui.EditField_3.Value = 0;
-        gui.EditField_4.Value = 0;
-        gui.EditField_5.Value = 0;
-        gui.EditField_6.Value = 0;
-        while gui.PopUpMenu.Value == "Braccio"
-            qrobot = deg2rad([gui.EditField.Value, gui.EditField_2.Value, gui.EditField_3.Value, ... 
-                gui.EditField_4.Value, gui.EditField_5.Value]);
-            robot.model.plot(qrobot);
+
+switch gui.PopUpMenu_2.Value
+    case 'Sliders'
+        switch gui.PopUpMenu.Value
+            case 'UR3'
+                while gui.PopUpMenu.Value == "UR3"
+                    qur3 = deg2rad([gui.EditField.Value, gui.EditField_2.Value, gui.EditField_3.Value, ... 
+                     gui.EditField_4.Value, gui.EditField_5.Value, gui.EditField_6.Value]);
+                    ur3.model.animate(qur3);
+                    if gui.PopUpMenu_2.Value == "X, Y and Z Directions"
+                        break;
+                    end
+                end
+            case 'Braccio'
+                while gui.PopUpMenu.Value == "Braccio"
+                    qrobot = deg2rad([gui.EditField.Value, gui.EditField_2.Value, gui.EditField_3.Value, ... 
+                        gui.EditField_4.Value, gui.EditField_5.Value]);
+                    robot.model.animate(qrobot);
+                end
         end
         
+    case 'X, Y and Z Directions'
+        switch gui.PopUpMenu.Value
+            case 'UR3'
+                while gui.PopUpMenu.Value == "UR3"
+                    tr = ur3.model.fkine(qur3) * transl (gui.EditFieldX.Value, gui.EditFieldY.Value,gui.EditFieldZ.Value);
+                    qur3_2 = ur3.model.ikcon(tr);
+                    ur3.model.animate(qur3_2);
+                    qur3 = qur3_2;
+                end
+            case 'Braccio'
+                
+        end
 end
