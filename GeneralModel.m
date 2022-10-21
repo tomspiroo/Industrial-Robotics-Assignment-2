cfb21758c195f584d2f6526e9723a7806a25a4c6
jeremyjmansfield-UTS <jeremy.j.mansfield@student.uts.edu.au>
2022-10-21 13:05:31 +1100

    Add Benchtop/Wall Models and General Model Code
    Add Benchtop/Wall Models and General Model Code

---
+++ GeneralModel.m
@@ -0,0 +1,46 @@
+classdef GeneralModel < handle
+    %MODIFIED FROM ROBOTICS TOOLBOX ROBOTCOWS
+    
+    properties (Constant)
+        
+    end
+    
+    properties
+        model;
+        location = transl(0,0,0);
+        name = 'name';
+        plyname;
+        workspace = [-3 0.5 -1.5 1.5 -1 2];
+    end
+    
+    methods
+%% MAIN FUNCTION. CREATES GRIPPER
+function self = GeneralModel(name,plyname,location,workspace)
+    if 0 < nargin
+        self.location = location;
+        self.name = name;
+        self.plyname = plyname;
+        self.workspace = workspace;
+    end
+    self.model = self.GetModel(name,plyname);
+    self.model.base = location;
+    plot3d(self.model,0,'workspace',workspace,'view',[-30,30],'delay',0);
+    hold on
+end
+    end
+    
+    methods (Static)
+%% Get Model Function (MODIFIED FROM ROBOTICS TOOLBOX ROBOTCOWS SCRIPT)
+function model = GetModel(name,plyName)
+    if nargin < 1
+        name = 'part';
+    end
+    [faceData,vertexData] = plyread(plyName,'tri');
+    L1 = Link('alpha',0,'a',0,'d',0.21,'offset',0);
+    model = SerialLink(L1,'name',name);
+    model.faces = {faceData,[]};
+    vertexData(:,2) = vertexData(:,2) + 0;
+    model.points = {vertexData,[]};
+end
+    end
+end
