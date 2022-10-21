4c38b42b378b1f49e35ea3205a623ad123148193
jeremyjmansfield-UTS <jeremy.j.mansfield@student.uts.edu.au>
2022-10-21 15:29:08 +1100

    Rewrite GeneralModel for Colour
    Rewrite GeneralModel for Colour

--- GeneralModel.m
+++ GeneralModel.m
@@ -1,46 +1,73 @@
 classdef GeneralModel < handle
-    %MODIFIED FROM ROBOTICS TOOLBOX ROBOTCOWS
-    
-    properties (Constant)
-        
-    end
-    
     properties
+        %> Robot model
         model;
-        location = transl(0,0,0);
+        
+        %> workspace
+        workspace = [-0.6 0.6 -0.6 0.6 -0.2 1.1];   
         name = 'name';
         plyname;
-        workspace = [-3 0.5 -1.5 1.5 -1 2];
+        location = transl(0,0,0);
     end
     
-    methods
-%% MAIN FUNCTION. CREATES GRIPPER
-function self = GeneralModel(name,plyname,location,workspace)
-    if 0 < nargin
-        self.location = location;
-        self.name = name;
-        self.plyname = plyname;
-        self.workspace = workspace;
-    end
-    self.model = self.GetModel(name,plyname);
-    self.model.base = location;
-    plot3d(self.model,0,'workspace',workspace,'view',[-30,30],'delay',0);
-    hold on
-end
-    end
-    
-    methods (Static)
-%% Get Model Function (MODIFIED FROM ROBOTICS TOOLBOX ROBOTCOWS SCRIPT)
-function model = GetModel(name,plyName)
-    if nargin < 1
-        name = 'part';
-    end
-    [faceData,vertexData] = plyread(plyName,'tri');
-    L1 = Link('alpha',0,'a',0,'d',0.21,'offset',0);
-    model = SerialLink(L1,'name',name);
-    model.faces = {faceData,[]};
-    vertexData(:,2) = vertexData(:,2) + 0;
-    model.points = {vertexData,[]};
-end
+    methods%% Class for General Model simulation
+        function self = GeneralModel(name,plyname,location,workspace)
+%             if 0 < nargin
+%                 if length(toolModelAndTCPFilenames) ~= 2
+%                     error('Please pass a cell with two strings, toolModelFilename and toolCenterPointFilename');
+%                 end
+%                 self.toolModelFilename = toolModelAndTCPFilenames{1};
+%                 self.toolParametersFilenamure = toolModelAndTCPFilenames{2};
+%             end
+        if 0 < nargin
+            self.location = location;
+            self.name = name;
+            self.plyname = plyname;
+            self.workspace = workspace;
+        end
+        self.GetModel();
+        self.PlotAndColourModel();%robot,workspace);
+        drawnow
+        end
+
+        %% GetUR3Robot
+        % Given a name (optional), create and return a UR3 robot model
+        function GetModel(self)
+            pause(0.001);
+            L1 = Link('d',0,'a',0.1,'alpha',0,'offset',0);
+            self.model = SerialLink(L1,'name',self.name);
+        end
+        %% PlotAndColourRobot
+        % Given a robot index, add the glyphs (vertices and faces) and
+        % colour them in if data is available 
+        function PlotAndColourModel(self)%robot,workspace)
+            for linkIndex = 0:self.model.n
+                [ faceData, vertexData, plyData{linkIndex + 1} ] = plyread([self.plyname],'tri'); %#ok<AGROW>                
+                self.model.faces{linkIndex + 1} = faceData;
+                self.model.points{linkIndex + 1} = vertexData;
+            end
+
+            % Display robot
+            self.model.plot3d(zeros(1,self.model.n),'noarrow','workspace',self.workspace);
+            if isempty(findobj(get(gca,'Children'),'Type','Light'))
+                camlight
+            end  
+            self.model.delay = 0;
+
+            % Try to correctly colour the arm (if colours are in ply file data)
+            for linkIndex = 0:self.model.n
+                handles = findobj('Tag', self.model.name);
+                h = get(handles,'UserData');
+                try 
+                    h.link(linkIndex+1).Children.FaceVertexCData = [plyData{linkIndex+1}.vertex.red ...
+                                                                  , plyData{linkIndex+1}.vertex.green ...
+                                                                  , plyData{linkIndex+1}.vertex.blue]/255;
+                    h.link(linkIndex+1).Children.FaceColor = 'interp';
+                catch ME_1
+                    disp(ME_1);
+                    continue;
+                end
+            end
+        end        
     end
 end
